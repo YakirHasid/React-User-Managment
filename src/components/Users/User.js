d3df91395792219978cb21daca6dfe77450a9925
YakirHasid <hasidyakir@gmail.com>
2023-10-02 18:25:53 +0300

    implementation of basic user component (static)

---
+++ src/components/Users/User.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./User.module.css";
+
+const User = () => {
+    return (
+        <div className={styles.user}>        
+            <h2>Max (31 years old)</h2>
+        </div>
+    )
+}
+
+export default User;
