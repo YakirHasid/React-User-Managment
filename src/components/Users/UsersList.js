7ccca7e75788509942b34b8cf9c16296817581a8
YakirHasid <hasidyakir@gmail.com>
2023-10-02 20:43:01 +0300

    wrap the users list with ul, li for each user

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,14 +1,20 @@
 import React from "react";
 import User from "./User";
 
+import styles from "./UsersList.module.css";
+
 const UsersList = (props) => {
-    return (
-        <div>
-            {props.items.map((user) => {
-                return <User key={user.id} name={user.name} age={user.age} />;
-            })}
-        </div>
-    );
+  return (
+    <ul>
+      {props.items.map((user) => {
+        return (
+          <li>
+            <User key={user.id} name={user.name} age={user.age} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default UsersList;
