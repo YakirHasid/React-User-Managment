fbd0d8c5bef066e26f2d233d7f7772fd4e02ee11
YakirHasid <hasidyakir@gmail.com>
2023-10-02 09:51:59 +0300

    use css module in component

--- src/UI/UserInput/UserInput.js
+++ src/UI/UserInput/UserInput.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import styles from "./UserInput.module.css";
+
 const UserInput = (props) => {
   return (
-    <div>
+    <div className={styles['user-input']}>
       <label htmlFor={props.id}>{props.label}</label>
       <input type="text" id={props.id} />
     </div>
