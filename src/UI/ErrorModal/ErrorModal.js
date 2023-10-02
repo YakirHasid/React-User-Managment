c42a6c825f1bcd0b4228765657fe90df2fa0fe7c
YakirHasid <hasidyakir@gmail.com>
2023-10-02 19:27:38 +0300

    dynamic error modal

---
+++ src/UI/ErrorModal/ErrorModal.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+  return (
+      <div className={styles["error-modal"]}>
+        <div className={styles["error-modal__elm"]}>
+          <h2>{props.title}</h2>
+        </div>
+        <div className={styles["error-modal__elm"]}>
+          <p>{props.message}</p>
+        </div>
+        <div className={styles["error-modal__elm"]}>
+          <button onClick={props.onConfirm}>Okay</button>
+        </div>
+      </div>
+  );
+};
+
+export default ErrorModal;
