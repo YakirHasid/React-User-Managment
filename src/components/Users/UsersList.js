f2771432f2a2402a32845df01a76e22ca4888428
YakirHasid <hasidyakir@gmail.com>
2023-10-02 18:27:04 +0300

    implementation of UsersList

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import User from './User';
+
+const UsersList = () => {
+    return (
+        <div>        
+            <User />  
+            <User />  
+            <User />  
+        </div>
+    )
+}
+
+export default UsersList;
