2eab56efa724f9969fc80b83886c59fa555ac5ac
YakirHasid <hasidyakir@gmail.com>
2023-10-02 18:25:37 +0300

    add the users list to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import AddUserForm from "./components/AddUserForm/AddUserForm.js";
 import Card from "./UI/Card/Card";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
     <div className="App">
       <Card>
         <AddUserForm />
+      </Card>
+      <Card>
+        <Users />
       </Card>
     </div>
   );
