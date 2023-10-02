907ba798d5acddeb2f264d55a51d31c858b2a0be
YakirHasid <hasidyakir@gmail.com>
2023-10-02 18:40:21 +0300

    implemented state for inputs, lift up on submit

--- src/components/AddUserForm/AddUserForm.js
+++ src/components/AddUserForm/AddUserForm.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import UserInput from "../../UI/UserInput/UserInput";
-import Button from "../../UI/Button/Button"
+import Button from "../../UI/Button/Button";
 
 const AddUserForm = (props) => {
+  const [userInput, setUserInput] = useState({ username: "", age: "" });
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    /* user input checks */
+    if (
+      userInput.username.trim().length === 0 ||
+      userInput.age.trim().length === 0
+    ) {
+      props.onError(
+        "Invalid input",
+        "Please enter a valid name and age (non-empty values)."
+      );
+      return;
+    }
+    if (+userInput.age < 1) {
+      props.onError("Invalid age", "Please enter a valid age (> 0).");
+      return;
+    }
+
+    /* add user after all checks passed successfully */
+    props.onAddUser(userInput);
+    setUserInput({ username: "", age: "" });
+  };
+
+  const userInputChangeHandler = (event) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [event.target.id]: event.target.value,
+      };
+    });
+  };
+
   return (
-    <form>
-        <UserInput id="username-field" label="Username" />
-        <UserInput id="age-field" label="Age (Years)" />        
-        <Button>Add User</Button>
+    <form onSubmit={submitHandler}>
+      <UserInput
+        onChange={userInputChangeHandler}
+        value={userInput.username}
+        id="username-field"
+        label="Username"
+      />
+      <UserInput
+        onChange={userInputChangeHandler}
+        value={userInput.age}
+        id="age-field"
+        label="Age (Years)"
+      />
+      <Button type="submit">Add User</Button>
     </form>
   );
 };
